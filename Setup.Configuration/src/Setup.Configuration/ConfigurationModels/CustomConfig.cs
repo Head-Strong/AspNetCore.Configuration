505f954292095d24c9dc668b4f816dd7e302aa0e
Head-Strong <aditya.magotra@gmail.com>
2016-12-04 19:17:13 +0530

    Added Configuration Code

---
+++ Setup.Configuration/src/Setup.Configuration/ConfigurationModels/CustomConfig.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Setup.Configuration.ConfigurationModels
+{
+    public class CustomConfig
+    {
+        public string WelcomeMessage { get; set; }  
+
+        public string Enviornment { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
--- Setup.Configuration/src/Setup.Configuration/Startup.cs
+++ Setup.Configuration/src/Setup.Configuration/Startup.cs
@@ -1,17 +1,40 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Setup.Configuration.ConfigurationModels;
 
 namespace Setup.Configuration
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional:true, reloadOnChange:true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public IConfigurationRoot Configuration { get; }
+
+        public IOptions<CustomConfig> CustomConfig { get; private set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CustomConfig>(Configuration.GetSection("CustomConfig"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,10 +47,14 @@ namespace Setup.Configuration
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            CustomConfig = (IOptions<CustomConfig>) app.ApplicationServices.GetService(typeof (IOptions<CustomConfig>));
+
+            app.Run(DisplayMessages);
         }
+
+        private async Task DisplayMessages(HttpContext context)
+        {
+            await context.Response.WriteAsync(CustomConfig.Value.ToString());
+        }        
     }
 }
